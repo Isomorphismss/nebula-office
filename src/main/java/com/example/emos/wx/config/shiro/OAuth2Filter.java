38e788c19d039336c225a6d0a82060e71fcc48ed
Isomorphism <jw6699@nyu.edu>
2024-12-01 18:21:33 -0500

    added two methods to handle authentication success and failure cases

--- src/main/java/com/example/emos/wx/config/shiro/OAuth2Filter.java
+++ src/main/java/com/example/emos/wx/config/shiro/OAuth2Filter.java
@@ -1,6 +1,9 @@
 package com.example.emos.wx.config.shiro;
 
 import cn.hutool.core.util.StrUtil;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +13,14 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Scope("prototype")
@@ -43,15 +51,72 @@ public class OAuth2Filter extends AuthenticatingFilter {
     @Override
     protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
         HttpServletRequest req = (HttpServletRequest) request;
-        if (req.getMethod().equals(RequestMethod.OPTIONS.name())) {
+        if (req.getMethod().equals(RequestMethod.OPTIONS.name())) { // 跳过所有Options请求
             return true;
         }
         return false;
     }
 
     @Override
-    protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
+        resp.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
+
+        threadLocalToken.clear();
+
+        String token = getRequestToken(req);
+        if (StrUtil.isBlank(token)) {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            resp.getWriter().print("无效的令牌");
+            return false;
+        }
+        try {
+            jwtUtil.verifierToken(token);
+        } catch (TokenExpiredException e) {
+            if (redisTemplate.hasKey(token)) {
+                redisTemplate.delete(token);
+                int userId = jwtUtil.getUserId(token);
+                token = jwtUtil.createToken(userId);
+                redisTemplate.opsForValue().set(token, userId + "", cacheExpire, TimeUnit.DAYS);
+                threadLocalToken.setToken(token);
+            } else {
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                resp.getWriter().print("令牌已过期");
+                return false;
+            }
+        } catch (JWTDecodeException e) {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            resp.getWriter().print("无效的令牌");
+            return false;
+        }
+        boolean bool = executeLogin(request, response);
+        return bool;
+    }
+
+    @Override
+    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
+        resp.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
+        resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        try {
+            resp.getWriter().print(e.getMessage());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         return false;
+    }
+
+    @Override
+    public void doFilterInternal(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        super.doFilterInternal(request, response, chain);
     }
 
     private String getRequestToken(HttpServletRequest request) {
