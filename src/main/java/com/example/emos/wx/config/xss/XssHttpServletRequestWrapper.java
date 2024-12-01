1ac1bd64f1179fe4a5a99c230ac28e9f924c461d
Isomorphism <jw6699@nyu.edu>
2024-11-30 22:25:31 -0500

    added a method in XssHttpServletRequestWrapper.java to filter IO stream

--- src/main/java/com/example/emos/wx/config/xss/XssHttpServletRequestWrapper.java
+++ src/main/java/com/example/emos/wx/config/xss/XssHttpServletRequestWrapper.java
@@ -2,9 +2,14 @@ package com.example.emos.wx.config.xss;
 
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HtmlUtil;
+import cn.hutool.json.JSONUtil;
 
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -65,6 +70,57 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
             value = HtmlUtil.filter(value);
         }
         return value;
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        InputStream in = super.getInputStream();
+        InputStreamReader reader = new InputStreamReader(in, Charset.forName("UTF-8"));
+        BufferedReader buffer = new BufferedReader(reader);
+        StringBuffer body = new StringBuffer();
+        String line = buffer.readLine();
+        while (line != null) {
+            body.append(line);
+            line = buffer.readLine();
+        }
+        buffer.close();
+        reader.close();
+        in.close();
+        Map<String, Object> map = JSONUtil.parseObj(body.toString());
+        Map<String, Object> result = new LinkedHashMap<>();
+        for (String key : map.keySet()) {
+            Object val = map.get(key);
+            if (val instanceof String) {
+                if (!StrUtil.hasEmpty(val.toString())) {
+                    result.put(key, HtmlUtil.filter(val.toString()));
+                }
+            } else {
+                result.put(key, val);
+            }
+        }
+        String json = JSONUtil.toJsonStr(result);
+        ByteArrayInputStream bain = new ByteArrayInputStream(json.getBytes());
+        return new ServletInputStream() {
+            @Override
+            public int read() throws IOException {
+                return bain.read();
+            }
+
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+
+            @Override
+            public boolean isReady() {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener) {
+
+            }
+        };
     }
 
 }
