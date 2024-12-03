571031fbadb9e23a0ff4f6a5f89ac0e6f30c3470
Isomorphism <jw6699@nyu.edu>
2024-12-03 10:52:04 -0500

    added a DAO method to insert user into database

--- src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
+++ src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
@@ -3,9 +3,13 @@ package com.example.emos.wx.db.dao;
 import com.example.emos.wx.db.pojo.TbUser;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+
 @Mapper
 public interface TbUserDao {
 
     public boolean haveRootUser();
+
+    public int insert(HashMap param);
 
 }
