60f5ff19eb8697f8135c572b2230be3d07eef8ba
Isomorphism <jw6699@nyu.edu>
2024-12-04 11:27:00 -0500

    implemented the UserService interface and added a method to get the user's ex open id from temp auth id

---
+++ src/main/java/com/example/emos/wx/service/impl/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.emos.wx.service.impl;
+
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.example.emos.wx.db.dao.TbUserDao;
+import com.example.emos.wx.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+@Slf4j
+@Scope("prototype")
+public class UserServiceImpl implements UserService {
+
+    @Value("${wx.app-id}")
+    private String appId;
+
+    @Value("${wx.app-secret}")
+    private String appSecret;
+
+    @Autowired
+    private TbUserDao tbUserDao;
+
+    private String getOpenId(String code) {
+        String url = "https://api.weixin.qq.com/sns/jscode2session";
+        HashMap map = new HashMap();
+        map.put("appid", appId);
+        map.put("secret", appSecret);
+        map.put("js_code", code);
+        map.put("grant_type", "authorization_code");
+        String response = HttpUtil.post(url, map);
+        JSONObject json = JSONUtil.parseObj(response);
+        String openId = json.getStr("openid");
+        if (openId == null || openId.length() == 0) {
+            throw new RuntimeException("临时登陆凭证错误");
+        }
+        return openId;
+    }
+
+}
