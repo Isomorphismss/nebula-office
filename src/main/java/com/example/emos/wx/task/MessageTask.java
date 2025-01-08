48186c1bd935814fa02b2167f9330a911cc5e9ee
Isomorphism <jw6699@nyu.edu>
2025-01-08 16:46:11 -0500

    added Sync/Async method to send message to rabbitMQ Queue

--- src/main/java/com/example/emos/wx/task/MessageTask.java
+++ src/main/java/com/example/emos/wx/task/MessageTask.java
@@ -1,10 +1,18 @@
 package com.example.emos.wx.task;
 
+import com.example.emos.wx.db.pojo.MessageEntity;
+import com.example.emos.wx.exception.EmosException;
 import com.example.emos.wx.service.MessageService;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
 
 @Component
 @Slf4j
@@ -15,5 +23,27 @@ public class MessageTask {
 
     @Autowired
     private MessageService messageService;
+
+    public void send(String topic, MessageEntity entity) {
+        String id = messageService.insertMessage(entity);
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel();
+        ) {
+            channel.queueDeclare(topic, true, false, false, null);
+            HashMap map = new HashMap();
+            map.put("messageId", id);
+            AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().headers(map).build();
+            channel.basicPublish("", topic, properties, entity.getMsg().getBytes());
+            log.debug("消息发送成功");
+        } catch (Exception e) {
+            log.error("执行异常", e);
+            throw new EmosException("向MQ发送消息失败");
+        }
+    }
+
+    @Async
+    public void sendAsync(String topic, MessageEntity entity) {
+        send(topic, entity);
+    }
 
 }
