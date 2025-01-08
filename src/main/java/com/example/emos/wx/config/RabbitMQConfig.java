938337ee47c343bf60ae6911a1c49aeec90a2693
Isomorphism <jw6699@nyu.edu>
2025-01-08 16:36:30 -0500

    fixed a bug that the getFactory method is missing Bean annotation

--- src/main/java/com/example/emos/wx/config/RabbitMQConfig.java
+++ src/main/java/com/example/emos/wx/config/RabbitMQConfig.java
@@ -1,11 +1,13 @@
 package com.example.emos.wx.config;
 
 import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
+    @Bean
     public ConnectionFactory getFactory() {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("http://${ENV}");
