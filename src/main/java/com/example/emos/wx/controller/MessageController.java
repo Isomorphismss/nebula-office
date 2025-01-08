08f713fbf561f700ca9f78033536d11a7f1ec956
Isomorphism <jw6699@nyu.edu>
2025-01-08 15:10:14 -0500

    created MessageController and added a method to query message by page

---
+++ src/main/java/com/example/emos/wx/controller/MessageController.java
@@ -0,0 +1,36 @@
+package com.example.emos.wx.controller;
+
+import com.example.emos.wx.common.util.R;
+import com.example.emos.wx.config.shiro.JwtUtil;
+import com.example.emos.wx.controller.form.SearchMessageByPageForm;
+import com.example.emos.wx.service.MessageService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/message")
+@Api("消息模块网络接口")
+public class MessageController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/searchMessageByPage")
+    public R searchMessageByPage(@Valid @RequestBody SearchMessageByPageForm form, @RequestHeader("token") String token) {
+        int userId = jwtUtil.getUserId(token);
+        int page = form.getPage();
+        int length = form.getLength();
+        long start = (long) (page - 1) * length;
+        List<HashMap> list = messageService.searchMessageByPage(userId, start, length);
+        return R.ok().put("result", list);
+    }
+
+}
