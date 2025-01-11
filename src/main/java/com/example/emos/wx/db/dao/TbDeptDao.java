699012f0e97754cd6bb59934748baa0bf796711e
Isomorphism <jw6699@nyu.edu>
2025-01-11 16:09:07 -0500

    added a DAO method to get meeting worker's info

--- src/main/java/com/example/emos/wx/db/dao/TbDeptDao.java
+++ src/main/java/com/example/emos/wx/db/dao/TbDeptDao.java
@@ -3,17 +3,12 @@ package com.example.emos.wx.db.dao;
 import com.example.emos.wx.db.pojo.TbDept;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 @Mapper
 public interface TbDeptDao {
-    int deleteByPrimaryKey(Integer id);
 
-    int insert(TbDept record);
+    public ArrayList<HashMap> searchDeptMembers(String keyword);
 
-    int insertSelective(TbDept record);
-
-    TbDept selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(TbDept record);
-
-    int updateByPrimaryKey(TbDept record);
 }
