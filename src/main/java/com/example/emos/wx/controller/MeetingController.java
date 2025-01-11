9a48b586662ee4cef6b0f10804a504fb57337c27
Isomorphism <jw6699@nyu.edu>
2025-01-10 23:09:34 -0500

    added a MeetingController

---
+++ src/main/java/com/example/emos/wx/controller/MeetingController.java
@@ -0,0 +1,13 @@
+package com.example.emos.wx.controller;
+
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/meeting")
+public class MeetingController {
+
+
+
+}
