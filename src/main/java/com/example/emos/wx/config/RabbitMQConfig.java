6f620d516bb151109cc003212d17ed7bcad10dd5
Isomorphism <jw6699@nyu.edu>
2025-02-17 16:05:14 -0500

    environment variables adaptation in the config file

--- src/main/java/com/example/emos/wx/config/RabbitMQConfig.java
+++ src/main/java/com/example/emos/wx/config/RabbitMQConfig.java
@@ -1,17 +1,24 @@
 package com.example.emos.wx.config;
 
 import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
+    @Value("${rabbitmq.host}")
+    private String host;
+
+    @Value("${rabbitmq.port}")
+    private int port;
+
     @Bean
     public ConnectionFactory getFactory() {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("${ENV}");
-        factory.setPort(5672);
+        factory.setHost(host);
+        factory.setPort(port);
         return factory;
     }
 
